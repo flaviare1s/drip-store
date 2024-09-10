b4175c20be3dbf085670a735b289830e8645ab80
flaviare1s <flaviarster@gmail.com>
2024-09-10 16:13:09 -0300

    Adiciona Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-primary m-auto"></div>
+    </div>
+  )
+}
