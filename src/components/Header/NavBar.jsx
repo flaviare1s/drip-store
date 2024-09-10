dce461d919749c8dced7ddcdb8accda0a8d31a09
flaviare1s <flaviarster@gmail.com>
2024-09-10 12:21:55 -0300

    Ajusta estilo na NavBar

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
   return (
     <section className="flex justify-between w-[80vw] lg:w-full relative">
-      <nav className='w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[90vh] lg:h-auto'>
+      <nav className='w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[100vh] lg:h-auto'>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/'>Home</Link>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/products'>Produtos</Link>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/categories'>Categorias</Link>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/orders'>Meus pedidos</Link>
 
-        <div className='lg:hidden flex flex-col flex-grow items-center justify-end w-full'>
+        <div className='lg:hidden flex flex-col flex-grow items-center justify-center w-full mt-[90px]'>
           <hr className="border-1 border-light-gray-2 w-full" />
           <Link to='/login' className='bg-primary h-10 w-[248px] text-white rounded-lg block font-bold text-sm text-center p-2.5 hover:bg-tertiary my-5'>Entrar</Link>
           <Link className='underline whitespace-nowrap' to='/register'>Cadastre-se</Link>
