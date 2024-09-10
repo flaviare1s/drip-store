5497ea978e7a04791fd19b3bc6ef1b3e3f0c2952
flaviare1s <flaviarster@gmail.com>
2024-09-10 12:34:51 -0300

    Ajusta estilo da NavBar

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
   return (
     <section className="flex justify-between w-[80vw] lg:w-full relative">
-      <nav className='w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[100vh] lg:h-auto'>
+      <nav className='w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-4 lg:gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[100vh] lg:h-auto'>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/'>Home</Link>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/products'>Produtos</Link>
         <Link className='hover:underline hover:text-primary hover:font-bold' to='/categories'>Categorias</Link>
