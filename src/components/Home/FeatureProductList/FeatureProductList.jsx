1a69a86618c2b36290f1214d1f0feb38fb16d7fe
flaviare1s <flaviarster@gmail.com>
2024-09-11 10:46:51 -0300

    Ajustes na sessão de produtos

--- src/components/Home/FeatureProductList/FeatureProductList.jsx
+++ src/components/Home/FeatureProductList/FeatureProductList.jsx
@@ -10,24 +10,25 @@ export const FeaturedProductList = () => {
   }
 
   useEffect(() => {
-    carregarProdutos()
+    carregarProdutos();
   }, []);
 
   return (
-    <div className="text-Inter px-5 md:px-[100px] bg-light-gray-3 md:bg-purple-50 md:pb-[120px] pb-10">
+    <div className="text-Inter px-5 md:px-[100px] bg-light-gray-3 md:bg-purple-50 md:pb-[80px] pb-10">
       <div className="flex justify-between mb-5 items-center">
         <h1 className="font-bold md:text-2xl">Produtos em alta</h1>
         <a
-          className="text-primary font-semibold md:font-normal text-sm md:text-lg tracking-[.75px]"
+          className="text-primary font-semibold md:font-normal text-sm md:text-lg tracking-[.75px] hover:font-bold"
           href="#"
         >
-          Ver todos
+          Ver todos →
         </a>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 rounded lg:grid-cols-4 bg-light-gray-3 md:bg-purple-50 gap-3 lg:gap-6 lg:gap-y-10">
-        {produtos && produtos.map((produto) => (
-          <ProductCard key={produto.id} produto={produto}/>
-        ))}
+        {produtos &&
+          produtos.map((produto) => (
+            <ProductCard key={produto.id} produto={produto} />
+          ))}
       </div>
     </div>
   );
