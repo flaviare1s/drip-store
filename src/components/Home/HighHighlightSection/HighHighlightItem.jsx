48e360e098b00f06d4a6df9e083852ff266adbd3
flaviare1s <flaviarster@gmail.com>
2024-09-12 10:05:51 -0300

    Corrige erro

--- src/components/Home/HighHighlightSection/HighHighlightItem.jsx
+++ src/components/Home/HighHighlightSection/HighHighlightItem.jsx
@@ -2,14 +2,24 @@
 export const HighHighlightItem = ({ title, imagem }) => {
   return (
     <article className="relative bg-purple rounded-lg p-5 flex justify-between flex-grow min-h-[212px] lg:min-h-[250px]">
-      <div className='w-1/2'>
-        <p className="bg-bright_yellow rounded-[30px] text-center text-xs font-bold py-2 w-[90px]">30% OFF</p>
-        <h2 className="text-[28px] font-bold leading-[36px] sm:text-[32px] sm:leading-[36px] py-2.5">{ title }</h2>
-        <button className="bg-white text-primary font-bold py-2 px-4 rounded-lg w-[145px] h-12 hover:bg-primary hover:text-white">Comprar</button>
+      <div className="w-1/2">
+        <p className="bg-bright_yellow rounded-[30px] text-center text-xs font-bold py-2 w-[90px]">
+          30% OFF
+        </p>
+        <h2 className="text-[28px] font-bold leading-[36px] sm:text-[32px] sm:leading-[36px] py-2.5">
+          {title}
+        </h2>
+        <button className="bg-white text-primary font-bold py-2 px-4 rounded-lg w-[145px] h-12 hover:bg-primary hover:text-white">
+          Comprar
+        </button>
       </div>
-      <div className='w-1/2'>
-        <img className='absolute right-0 bottom-0 lg:w-[242px]' src={imagem} alt="Camisa" />
+      <div className="w-1/2">
+        <img
+          className="absolute right-0 bottom-0 lg:w-[242px]"
+          src={imagem}
+          alt="Camisa"
+        />
       </div>
     </article>
-  )
-}
+  );
+};
