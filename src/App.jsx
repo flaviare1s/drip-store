533524bbcabd28d65e699270da933ed96c3d28f2
flaviare1s <flaviarster@gmail.com>
2024-09-15 20:22:57 -0300

    Corrige navegação (scroll to top)

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,56 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Header } from './components/Header/Header.jsx'
-import { Home } from './pages/Home.jsx'
-import { Footer } from './components/Footer.jsx'
-import { Login } from './pages/Login.jsx'
-import { Register } from './pages/Register.jsx'
-import { Toaster } from 'react-hot-toast'
-import { UserContext } from './contexts/UserContext.jsx'
-import { useEffect, useState } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { Loader } from './components/Loader.jsx'
-import { auth } from './firebase/config.js'
-import { ResetPassword } from './pages/ResetPassword.jsx'
-import { Products } from './pages/Products.jsx'
-import { Orders } from './pages/Orders.jsx'
-import { Product } from './pages/Product.jsx'
-import { CartCheckout } from './pages/CartCheckout.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header.jsx";
+import { Home } from "./pages/Home.jsx";
+import { Footer } from "./components/Footer.jsx";
+import { Login } from "./pages/Login.jsx";
+import { Register } from "./pages/Register.jsx";
+import { Toaster } from "react-hot-toast";
+import { UserContext } from "./contexts/UserContext.jsx";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { Loader } from "./components/Loader.jsx";
+import { auth } from "./firebase/config.js";
+import { ResetPassword } from "./pages/ResetPassword.jsx";
+import { Products } from "./pages/Products.jsx";
+import { Orders } from "./pages/Orders.jsx";
+import { Product } from "./pages/Product.jsx";
+import { CartCheckout } from "./pages/CartCheckout.jsx";
+import { ScrollToTop } from "./components/ScrollToTop.jsx";
 export function App() {
-  const [usuarioLogado, setUsuarioLogado] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setUsuarioLogado(user)
-      setLoading(false)
-    })
-  }, [])
+      setUsuarioLogado(user);
+      setLoading(false);
+    });
+  }, []);
 
   if (loading) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
     <>
-     <UserContext.Provider value={usuarioLogado}>
-       <BrowserRouter>
-       <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='reset-password' element={<ResetPassword />} />
-          <Route path='products' element={<Products />} />
-          <Route path='products/:id' element={<Product />} />
-          <Route path='/orders' element={<Orders />} />
-          <Route path='/checkout' element={<CartCheckout />} />
-        </Routes>
-        <Footer />
-       </BrowserRouter>
-       <Toaster position='top-center'/>
-     </UserContext.Provider>
+      <UserContext.Provider value={usuarioLogado}>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:id" element={<Product />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/checkout" element={<CartCheckout />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+        <Toaster position="top-center" />
+      </UserContext.Provider>
     </>
-  )
+  );
 }
--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,48 +1,82 @@
 /* eslint-disable react/prop-types */
-import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { UserContext } from "../../contexts/UserContext"
-import { Button } from "../Button"
-import { logout } from "../../firebase/auth"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { Button } from "../Button";
+import { logout } from "../../firebase/auth";
 
 export const NavBar = ({ setIsMenuOpen }) => {
-  const user = useContext(UserContext)
+  const user = useContext(UserContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleCloseMenu = () => {
-    setIsMenuOpen(false)
-  }
+    setIsMenuOpen(false);
+  };
 
   function handleLogout() {
     logout().then(() => {
-      navigate("/login")
-    })
+      navigate("/login");
+    });
   }
 
   return (
     <section className="flex justify-between w-[80vw] lg:w-full relative">
-      <nav className='w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-4 lg:gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[100vh] lg:h-auto'>
-        <Link onClick={handleCloseMenu} className='hover:underline hover:text-primary hover:font-bold' to='/'>Home</Link>
-        <Link onClick={handleCloseMenu} className='hover:underline hover:text-primary hover:font-bold' to='/products'>Produtos</Link>
-        <Link onClick={handleCloseMenu} className='hover:underline hover:text-primary hover:font-bold' to='/#categories'>Categorias</Link>
-        <Link onClick={handleCloseMenu} className='hover:underline hover:text-primary hover:font-bold' to='/orders'>Meus pedidos</Link>
+      <nav className="w-full flex flex-col lg:flex-row lg:justify-start items-start lg:items-center gap-4 lg:gap-8 text-dark-gray-2 leading-[28px] tracking-[0.75px] absolute lg:relative top-0 left-[-20px] z-50 bg-white p-[30px] lg:p-0 h-[100vh] lg:h-auto">
+        <Link
+          onClick={handleCloseMenu}
+          className="hover:underline hover:text-primary hover:font-bold"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          onClick={handleCloseMenu}
+          className="hover:underline hover:text-primary hover:font-bold"
+          to="/products"
+        >
+          Produtos
+        </Link>
+        <Link
+          onClick={handleCloseMenu}
+          className="hover:underline hover:text-primary hover:font-bold"
+          to="/#categories"
+        >
+          Categorias
+        </Link>
+        <Link
+          onClick={handleCloseMenu}
+          className="hover:underline hover:text-primary hover:font-bold"
+          to="/orders"
+        >
+          Meus pedidos
+        </Link>
 
         {!user && (
-        <div className='lg:hidden flex flex-col flex-grow items-center justify-center w-full mt-[90px]'>
-          <hr className="border-1 border-light-gray-2 w-full" />
-          <Link to='/login' className='bg-primary h-10 w-[248px] text-white rounded-lg block font-bold text-sm text-center p-2.5 hover:bg-tertiary my-5'
-          onClick={handleCloseMenu}>Entrar</Link>
-          <Link className='underline whitespace-nowrap hover:text-primary hover:font-bold' to='/register'
-          onClick={handleCloseMenu}>Cadastre-se</Link>
-        </div>
+          <div className="lg:hidden flex flex-col flex-grow items-center justify-center w-full mt-[90px]">
+            <hr className="border-1 border-light-gray-2 w-full" />
+            <Link
+              to="/login"
+              className="bg-primary h-10 w-[248px] text-white rounded-lg block font-bold text-sm text-center p-2.5 hover:bg-tertiary my-5"
+              onClick={handleCloseMenu}
+            >
+              Entrar
+            </Link>
+            <Link
+              className="underline whitespace-nowrap hover:text-primary hover:font-bold"
+              to="/register"
+              onClick={handleCloseMenu}
+            >
+              Cadastre-se
+            </Link>
+          </div>
         )}
 
         {user && (
           <div className="lg:hidden flex flex-col flex-grow items-center justify-center w-full mt-[90px]">
-            <Button title='Logout' onClick={handleLogout} />
+            <Button title="Logout" onClick={handleLogout} />
           </div>
         )}
       </nav>
     </section>
-  )
-}
+  );
+};
