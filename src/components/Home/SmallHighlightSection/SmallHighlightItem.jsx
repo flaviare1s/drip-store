229486e6056d928476ff6685d0932be82c93ff9f
flaviare1s <flaviarster@gmail.com>
2024-09-16 15:21:22 -0300

    Altera "a" por "Link" em SmallLightSection.jsx

--- src/components/Home/SmallHighlightSection/SmallHighlightItem.jsx
+++ src/components/Home/SmallHighlightSection/SmallHighlightItem.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom"
+
 /* eslint-disable react/prop-types */
 export const SmallHighlightItem = ({image, name, ctaLink}) => {
   return (
     <div>
-        <a href={ctaLink}>
+        <Link to={ctaLink}>
           <div className="flex flex-col justify-center items-center">
             <div className="bg-white p-5 rounded-full shadow">
               <div className="min-w-[56px] aspect-square :text-Primary">{image}</div>
             </div>
             <p className="text-dark-gray-2 text-xs md:text:sm font-bold leading-[22px] mt-3 tracking-[1px] md:tracking-tighter[.75px]">{name}</p>
           </div>
-        </a>
+        </Link>
       </div>
   )
 }
